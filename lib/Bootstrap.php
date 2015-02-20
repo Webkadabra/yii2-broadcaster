0e96c793e0fc39a9a4ba0ca8caa96c66e20d9a4c
Jake Oehler Morrison <jomorrison@gmail.com>
2015-02-20 13:34:49 -0800

    Create Bootstrap.php

---
+++ lib/Bootstrap.php
@@ -0,0 +1,15 @@
+<?php
+namespace infinite\notification;
+use Yii;
+use yii\base\BootstrapInterface;
+use yii\base\Application;
+use yii\base\Event;
+use infinite\base\Daemon;
+use infinite\base\Cron;
+class Bootstrap implements BootstrapInterface
+{
+public function bootstrap($app)
+{
+
+}
+}
