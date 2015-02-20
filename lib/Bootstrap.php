d91b9f247c8c6c56cc45b47300bed88ae64c71dd
Jake Oehler Morrison <jmorrison@psesd.org>
2015-02-20 13:46:04 -0800

    formatting updates

--- lib/Bootstrap.php
+++ lib/Bootstrap.php
@@ -6,10 +6,11 @@ use yii\base\Application;
 use yii\base\Event;
 use infinite\base\Daemon;
 use infinite\base\Cron;
+
 class Bootstrap implements BootstrapInterface
 {
-public function bootstrap($app)
-{
+	public function bootstrap($app)
+	{
 
-}
+	}
 }
