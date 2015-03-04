33caa513664447ace9d03b111d95500f0c86b302
Jake Oehler Morrison <jmorrison@psesd.org>
2015-03-04 14:22:00 -0800

    formatting update

--- lib/Bootstrap.php
+++ lib/Bootstrap.php
@@ -1,16 +1,11 @@
 <?php
 namespace infinite\notification;
-use Yii;
+
 use yii\base\BootstrapInterface;
-use yii\base\Application;
-use yii\base\Event;
-use infinite\base\Daemon;
-use infinite\base\Cron;
 
 class Bootstrap implements BootstrapInterface
 {
-	public function bootstrap($app)
-	{
-
-	}
+    public function bootstrap($app)
+    {
+    }
 }
