e413a218961b908c72772765f3f3607a8f265ed8
Jake Oehler Morrison <jmorrison@psesd.org>
2015-03-04 18:37:52 -0800

    doc fixes

--- lib/Bootstrap.php
+++ lib/Bootstrap.php
@@ -4,8 +4,16 @@ namespace infinite\notification;
 
 use yii\base\BootstrapInterface;
 
+/**
+ * Bootstrap [[@doctodo class_description:infinite\notification\Bootstrap]].
+ *
+ * @author Jacob Morrison <email@ofjacob.com>
+ */
 class Bootstrap implements BootstrapInterface
 {
+    /**
+     * [[@doctodo method_description:bootstrap]].
+     */
     public function bootstrap($app)
     {
     }
