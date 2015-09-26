c6990cb6dcd5ebb7cb5073b9c68d97ad6c78eaf8
Jake Oehler Morrison <jmorrison@psesd.org>
2015-09-25 20:12:14 -0700

    minor fix

--- lib/emails/default/one_rich.php
+++ lib/emails/default/one_rich.php
@@ -8,7 +8,7 @@ $meta = $eventType->getMeta($event);
 echo Html::beginTag('div', ['class' => 'list-group-item']);
 echo Html::beginTag('h4', ['class' => 'list-group-item-heading']);
 echo $meta['descriptor'];
-echo Html::endTag('div');
+echo Html::endTag('h4');
 echo Html::beginTag('div', ['class' => 'list-group-item-text']);
 echo Html::tag('small', $meta['created_human']);
 echo Html::endTag('div');
